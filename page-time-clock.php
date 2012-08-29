cd2ee560b303dd900bf49830c7526c7e64fe3d50
Zach Zimbler <zachzimbler@gmail.com>
2012-08-29 16:19:51 -0400

    update

---
+++ page-time-clock.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Full Width Page
+*/
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix row-fluid">
+			
+				<div id="main" class="span12 clearfix" role="main">
+<?	
+					$ipaddress = $_SERVER['REMOTE_ADDR'];
+					$error = rand(0, 2);
+					echo $ipaddress;
+					if ($ipaddress == '128.118.17.253'){
+
+						echo "<iframe src='https://docs.google.com/spreadsheet/embeddedform?formkey=dGQxbHg1MmxiQW9KeHlJd1BHMjMyYkE6MQ' width='760' height='900' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe>";	
+
+					} else {
+						if ($error == 0)
+						{
+							echo '<img src="http://s13.postimage.org/y3ozd8xuf/ERMAGHERDBERNERNERS.jpg" alt="lol get a life" />';
+							echo '<p>OH, YOU THOUGHT THERE WAS SOMETHING RELEVANT HERE?</p>';
+						} else if($error == 1) {
+							echo '<img src="http://s11.postimage.org/qw70uxver/Perfect_World.jpg" alt="lol get a life" />';
+							echo '<p>SORRY, WHAT EXACTLY WERE YOU DOING? CLOCKING IN?</p>';
+						} else {
+							echo '<img src="http://www.withinwindows.com/files/uploads/2012/05/ming.png " alt="lol get a life" />';
+							echo '<p>DID YOU TYPE THE RIGHT URL? PROBABLY LOL</p>';
+						}
+					}
+?>						
+				</div> <!-- end #main -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
