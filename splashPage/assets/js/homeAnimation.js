14509edba6497ea09731d1237ed1e1c96fcee5e2
Zach Zimbler <zachzimbler@gmail.com>
2012-08-29 00:26:32 -0400

    udpates to the splashpage

--- splashPage/assets/js/homeAnimation.js
+++ splashPage/assets/js/homeAnimation.js
@@ -1,11 +1,13 @@
 $(function () {
 	$("#quotes").fadeIn(1250, function() {
-		$("#winc").animate({"left": "226px"},1000,"linear",function() {
-			$("#homeLinks").show()
-			$("#homeLinks").animate({"margin-top": "60px"},300,"linear", function(){
-				$("#splashSocial").fadeIn('slow')			
-				
-			})		
+		$("#world").animate({"left": "200px"},1000,"linear",function() {
+			$("#inConversation").animate({"left": "354px"}, 1000, "linear", function(){	
+				$("#homeLinks").show(function(){
+					$("#homeLinks").animate({"margin-top": "0px"},300,"linear", function(){
+						$("#splashSocial").fadeIn('slow')			
+					})		
+				})
+			})
 		})
 	})	
 });
