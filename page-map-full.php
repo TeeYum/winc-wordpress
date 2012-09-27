60c410dd6f6a9bdc512d1adf0b7dd07e8ec32f0f
Zach Zimbler <zachzimbler@gmail.com>
2012-09-27 15:07:44 -0400

    daasdf

--- page-map-full.php
+++ page-map-full.php
@@ -12,10 +12,11 @@ Template Name: Map of Dialogues
 				</div>	
 				
 				<div id="map-main" class="span12 well dropShadow" >
-				<button class="btn btn-large btn-danger" rel="popover" data-trigger="click" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">Click to toggle popover</button>				
+				<button class="btn btn-large btn-danger" rel="popover" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">Click to toggle popover</button>				
 				</div> <!-- end #main -->
     
 				<?php //get_sidebar(); // sidebar 1 ?>
     
 			</div> <!-- end #content -->
+			<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/maps.js" ></script>
 <?php get_footer(); ?>
