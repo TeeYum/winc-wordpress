f4f162d95b2919bec57b480818465efdd770ce57
Zach Zimbler <zachzimbler@gmail.com>
2012-09-27 14:03:03 -0400

    made sure the map page will align properly

--- page-map-full.php
+++ page-map-full.php
@@ -7,11 +7,11 @@ Template Name: Map of Dialogues
 <?php get_header(); ?>
 			
 			<div id="content" class="clearfix row-fluid">
-				<div id="dialogue-map" class="span12 well dropShadow">
+				<div id="map-hero" class="span12 well dropShadow">
 					<img src="<?php bloginfo('template_directory'); ?>/images/whomap.png" alt="picture" />
 				</div>	
 				
-				<div id="" class="span12 well dropShadow" >
+				<div id="map-main" class="span12 well dropShadow" >
 					<a href="#" class="btn btn-large btn-danger" rel="popover" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">Click to toggle popover</a>	
 				</div> <!-- end #main -->
     
