25c118f668608436ce17424eef4be4876e656c75
Zach Zimbler <zachzimbler@gmail.com>
2012-10-02 12:48:42 -0400

    udasdf

--- page-face-face-full.php
+++ page-face-face-full.php
@@ -9,14 +9,16 @@ Template Name: Face to Face
 			<div id="content" class="clearfix row-fluid">
 				<div id="face-face-main" class="span12 well dropShadow" >
 					<img src="<?php bloginfo('template_directory'); ?>/images/facetofaceheader.png" alt="Face to Face Conversations" />
-					<div class="span6">
-						<div class="face-to-face-title-box">
-							<h2>US Race Relations</h2>
+					<div class="row">
+						<div class="span6">
+							<div class="face-to-face-title-box">
+								<h2>US Race Relations</h2>
+							</div>
 						</div>
-					</div>
-					<div class="span6">
-						<div class="face-to-face-title-box">
-							<h2>Gender</h2>
+						<div class="span6">
+							<div class="face-to-face-title-box">
+								<h2>Gender</h2>
+							</div>
 						</div>
 					</div>
 				</div> <!-- end #main -->
