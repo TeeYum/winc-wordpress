359d5158dfcee6385beaca764535f4ffd42021aa
Zach Zimbler <zachzimbler@gmail.com>
2012-10-02 17:23:26 -0400

    made the front page animation 2 times faster

--- splashPage/assets/js/homeAnimation.js
+++ splashPage/assets/js/homeAnimation.js
@@ -1,7 +1,7 @@
 $(function () {
 	$("#quotes").fadeIn(1250, function() {
-		$("#world").animate({"left": "200px"},1000,"linear",function() {
-			$("#inConversation").animate({"left": "354px"}, 1000, "linear", function(){	
+		$("#world").animate({"left": "200px"},500,"linear",function() {
+			$("#inConversation").animate({"left": "354px"}, 500, "linear", function(){	
 				$("#homeLinks").show(function(){
 					$("#homeLinks").animate({"margin-top": "0px"},300,"linear", function(){
 						$("#splashSocial").fadeIn('slow')			
