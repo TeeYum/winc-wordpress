5df07027bd15033cbdb7f73794672ec4a43b27ac
Zach Zimbler <zachzimbler@gmail.com>
2012-10-02 01:40:27 -0400

    added facet o face stuff

---
+++ page-face-face-full.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Face to Face
+ */
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix row-fluid">
+				<div id="face-face-main" class="span12 well dropShadow" >
+					<img src="<?php bloginfo('template_directory'); ?>/images/facetofaceheader.png" alt="Face to Face Conversations" />
+					<div class="span6">
+					</div>
+					<div class="span6">
+				
+					</div>
+				</div> <!-- end #main -->
+			</div> <!-- end #content -->
+<?php get_footer(); ?>
