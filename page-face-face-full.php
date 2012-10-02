7404c47e63cd3ca2476fc49bcfff25f7150b01e6
Zach Zimbler <zachzimbler@gmail.com>
2012-10-02 13:20:53 -0400

    asdafsdf

--- page-face-face-full.php
+++ page-face-face-full.php
@@ -14,6 +14,11 @@ Template Name: Face to Face
 							<div class="face-to-face-title-box">
 								<h2>US Race Relations</h2>
 							</div>
+							<p>U.S. Race Relations began as – and continues to be – the core of our work and mission. We, as Americans, live largely segregated lives. We rarely understand the differences between us, and we tend to think in ways that keep legacies of distrust alive. As a consequence, race has become taboo, a topic to be avoided. When race is discussed publicly, it is all too often with the oversight of simplistic attitudes about political correctness.
+
+In our Race Relations dialogues, we provide an ideologically neutral space where participants are able to explore their thoughts, ideas, and questions about race, no matter what they are. We challenge people of all ancestries to examine the biases, the rumors, and the inherited scripts we all live with, and to listen to the stories of people who appear to be on the other side.
+
+These conversations, which typically involve eight participants, are facilitated by two trained facilitators. The facilitators use their training to create a space where all perspectives – from the far left to the far right – are unearthed and discussed.</p>
 						</div>
 						<div class="span6">
 							<div class="face-to-face-title-box">
