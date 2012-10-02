e569a95abe538ed2ce8594684d9a9bf3d34398d0
Zach Zimbler <zachzimbler@gmail.com>
2012-10-02 12:13:32 -0400

    added what we do

---
+++ page-what-we-do-full.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: What We Do
+ */
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix row-fluid">
+				<div id="whatwedo-main" class="span12 well dropShadow" >
+					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo.png" alt="What We Do" />
+				</div> <!-- end #main -->
+			</div> <!-- end #content -->
+<?php get_footer(); ?>
