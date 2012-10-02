0094984c83c5e9f7b89882d88f7d0f25c7eaa34e
Zach Zimbler <zachzimbler@gmail.com>
2012-10-02 11:39:39 -0400

    face to face thing

--- page-face-face-full.php
+++ page-face-face-full.php
@@ -10,6 +10,9 @@ Template Name: Face to Face
 				<div id="face-face-main" class="span12 well dropShadow" >
 					<img src="<?php bloginfo('template_directory'); ?>/images/facetofaceheader.png" alt="Face to Face Conversations" />
 					<div class="span6">
+						<div class="face-to-face-title-box">
+							<h2>US Race Relations</h2>
+						</div>
 					</div>
 					<div class="span6">
 				
