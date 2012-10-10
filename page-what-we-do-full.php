b9f7a30e241f1c50edc9b3aee0cb86b479845590
TeeYum <tnt5027@gmail.com>
2012-10-10 12:28:31 -0300

    Join a Conversation clickable

--- page-what-we-do-full.php
+++ page-what-we-do-full.php
@@ -12,7 +12,7 @@ Template Name: What We Do
 				</div>
 			<div id="content" class="clearfix row-fluid">
 				<div id="whatwedo-main" class="span12 well dropShadow" >
-					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-bottom.png" alt="Click to Join a Conversation" style="margin-left:30px;" />
+					<a href="http://www.worldinconversation.org/sorry-closed/"><img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-bottom.png" alt="Click to Join a Conversation" style="margin-left:30px;" /></a>
 				</div> <!-- end #main -->
 			</div> <!-- end #content -->
 <?php get_footer(); ?>
