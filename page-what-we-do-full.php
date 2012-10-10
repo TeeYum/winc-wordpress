6ba293bf103ca57fbfeca115f1ddb2f6b8bcfa79
TeeYum <tnt5027@gmail.com>
2012-10-10 11:42:17 -0300

    margins...

--- page-what-we-do-full.php
+++ page-what-we-do-full.php
@@ -11,7 +11,7 @@ Template Name: What We Do
 					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-top.png" alt="What We Do" style="margin-left:30px;" />
 				</div> <!-- end #main -->
 			<div id="whatwedo-main" class="span12 well dropShadow" >
-					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-bottom.png" alt="Click to Join a Conversation" style="margin-left:30px;" />
+					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-bottom.png" alt="Click to Join a Conversation" style="margin-left:15px;" />
 				</div> <!-- end #main -->
 			</div> <!-- end #content -->
 <?php get_footer(); ?>
