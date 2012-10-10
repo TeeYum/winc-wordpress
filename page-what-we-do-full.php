a2c5bd2ad52af1b22a4ae2ca01937069011bebca
TeeYum <tnt5027@gmail.com>
2012-10-10 11:41:12 -0300

    Added Join a Conversation

--- page-what-we-do-full.php
+++ page-what-we-do-full.php
@@ -10,5 +10,8 @@ Template Name: What We Do
 				<div id="whatwedo-main" class="span12 well dropShadow" >
 					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-top.png" alt="What We Do" style="margin-left:30px;" />
 				</div> <!-- end #main -->
+			<div id="whatwedo-main" class="span12 well dropShadow" >
+					<img src="<?php bloginfo('template_directory'); ?>/images/whatwedo-bottom.png" alt="Click to Join a Conversation" style="margin-left:30px;" />
+				</div> <!-- end #main -->
 			</div> <!-- end #content -->
 <?php get_footer(); ?>
