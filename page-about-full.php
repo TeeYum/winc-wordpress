2c726e924edfd1353a4c2dcfaeb1bac982e5018a
TeeYum <tnt5027@gmail.com>
2012-10-18 11:14:30 -0300

    sam bio

--- page-about-full.php
+++ page-about-full.php
@@ -36,7 +36,7 @@ Template Name: About Page Template
 							</div>
 							<div class="back">
 									<h3 class="about-name">Sam Richards</h3>
-									<p>I studied English as an undergraduate at Rutgers, social work at the Master’s level at Rutgers, and Human Development and Family Studies at the doctoral level at Penn State.  I’m not sure what all of that makes me.  But the common denominator seems like it’s always been an attempt to understand people and Life, and to articulate what I discover.</p>	
+									<p>Sam is a sociologist currently working at The Pennsylvania State University. His work focuses on race and ethnicity. He co-directs the World in Conversation Project, as well as teaches the largest race course in the country with 720 students per semester. As a professor he is well known on campus and his iconoclasm landed him in “The Professors: The 101 Most Dangerous Academics in America,” a book published by David Horowitz that critiques the dominance of liberal thinking in the academy–which is ironic given that he is a registered Libertarian. In addition, his TED talk (called a “Radical Experiment in Empathy”) is the third most widely viewed TEDx talk in the world as of May 2011.</p>	
 								</div>
 							</div>
 						</li>		
