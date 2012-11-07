3bef9c9fc8220d65934d8d2c7da9ec375d2a6071
TeeYum <tnt5027@gmail.com>
2012-11-07 16:28:17 -0500

    sizing..

--- header.php
+++ header.php
@@ -93,7 +93,7 @@
 				<div class="navbar navbar-fixed-top">
 					<div class="navbar-inner">
 						<div>
-							<a><img class="alignleft" src="http://s9.postimage.org/za93tq567/PSUGrey.png" alt="Test Logo" alt width="200" height="50" /></a>
+							<a><img class="alignleft" src="http://s9.postimage.org/za93tq567/PSUGrey.png" alt="Test Logo" alt width="180" height="60" /></a>
 						</div>
 						<div class="container-fluid nav-container">
 								<nav role="navigation">
