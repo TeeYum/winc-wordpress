763c56e63daef1fe2f569978b4fa01a0ae1568d2
Justin Sova <atzdrummer@gmail.com>
2014-02-24 14:02:45 -0500

    trying some html magic to fix the animation issues

--- splashPage/assets/js/homeAnimation.js
+++ splashPage/assets/js/homeAnimation.js
@@ -2,13 +2,13 @@ $(function() {
 	$("#quotes").fadeIn(1250, function() {
 		$("#world").animate({"left": "200px"}, 500, "linear", function() {
 			$("#inConversation").animate({"left": "354px"}, 500, "linear", function() {
-				//$("#centerForDiplomacy").animate({"left": "400px"}, 500, "linear", function() {
+				$("#centerForDiplomacy").animate({"left": "400px"}, 500, "linear", function() {
 					$("#homeLinks").show(function() {
 						$("#homeLinks").animate({"margin-top": "0px"}, 300, "linear", function() {
 							$("#splashSocial").fadeIn('slow')
 						})
 					})
-				//})
+				})
 			})
 		})
 	})
