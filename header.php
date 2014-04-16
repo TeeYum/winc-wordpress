1502ef6d9655d8c09e19378bae60afc68b5715e5
TeeYum <tnt5027@gmail.com>
2014-04-16 09:28:49 -0400

    Updated Analytics

--- header.php
+++ header.php
@@ -7,8 +7,21 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	
 	<head>
+	<!-- Google Analytics -->
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-xxxxx-y']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<!-- Normal Header stuffs -->	
 		
 		<title><?php bloginfo('name'); ?><?php wp_title('-', true, 'left'); ?></title>
 				
