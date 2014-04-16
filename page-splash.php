c87096beb06ea61c8a27a7d702d8e5b30c2e30ac
TeeYum <tnt5027@gmail.com>
2014-04-16 09:27:15 -0400

    Updated Analytics

--- page-splash.php
+++ page-splash.php
@@ -58,17 +58,17 @@ this is a stupid change
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="<?php echo get_template_directory_uri(); ?>/splashPage/assets/js/jquery.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/splashPage/assets/js/homeAnimation.js"></script>
-  		<script type="text/javascript">
+  		
+		<!--Google Analytics -->		
+		<script type="text/javascript">
 			var _gaq = _gaq || [];
-  			_gaq.push(['_setAccount', 'UA-29115203-1']);
+			_gaq.push(['_setAccount', 'UA-xxxxx-y']);
 			_gaq.push(['_trackPageview']);
-
-  			(function() {
-    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  			})();
-
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		</script>
 	</body>
 </html>
