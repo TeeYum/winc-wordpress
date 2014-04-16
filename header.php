5e59386ea176f32e620d5f79841e6a399b1a8b2f
TeeYum <tnt5027@gmail.com>
2014-04-16 10:19:07 -0400

    universal analytics header

--- header.php
+++ header.php
@@ -7,17 +7,6 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	
 	<head>
-	<!-- Google Analytics -->
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-XXXXXX-XX', 'example.com');
-			ga('require', 'displayfeatures');
-			ga('send', 'pageview');
-		</script>
-		
 		<!-- Normal Header stuffs -->
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -136,6 +125,16 @@
 						</div>
 					</div>
 				</div>
+			<!-- Google Analytics -->	
+			<script>
+  				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  				ga('create', 'UA-29115203-2', 'worldinconversation.org');
+  				ga('send', 'pageview');
+			</script>	
 			
 			</div> <!-- end #inner-header -->
 		
